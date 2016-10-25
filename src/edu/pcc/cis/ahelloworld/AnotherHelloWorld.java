464437964bc81fe59766bb11472ee0ac3db95769
Ryan Booker <ryan.m.booker@gmail.com>
2016-10-25 14:20:41 -0700

    initialize project

---
+++ src/edu/pcc/cis/ahelloworld/AnotherHelloWorld.java
@@ -0,0 +1,13 @@
+package edu.pcc.cis.ahelloworld;
+
+/**
+ * AnotherHelloWorld
+ * Description: A very simple class that prints out a few lines of text
+ *
+ * @Author: Ryan Booker
+ * @Version: 10.25.16
+ *
+ */
+public class AnotherHelloWorld {
+    
+}
